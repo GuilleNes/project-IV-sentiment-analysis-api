80a82802a9f6398559392c45c9b9a0b5a177d33f
Guillermo Nespral <gnsg32@gmail.com>
2022-11-13 19:24:40 +0100

    uploaded project

--- data/queen_sentiment.sql
+++ data/queen_sentiment.sql
@@ -15,5 +15,13 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/queen_sentiment.
         LINES TERMINATED BY '\r\n'
         IGNORE 1 ROWS;
         
-SELECT *
-FROM queen_sentiment;
+SELECT * 
+    FROM queen_sentiment
+    WHERE neu > 0.900;
+    
+SELECT ROUND(AVG(compound), 3) FROM queen_sentiment
+	WHERE tweet LIKE '%Harry%';
+    
+SELECT * FROM queen_sentiment;
+
+    
